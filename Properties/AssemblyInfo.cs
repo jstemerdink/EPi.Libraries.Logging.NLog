c477682569e6821eaefc35112e297f5fe26a53df
Jeroen <jeroen.developer@outlook.com>
2015-05-30 14:36:15 +0200

    Added Gitversion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -52,18 +52,3 @@ using EPiServer.Logging;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("b4287d75-cc5c-4b15-a151-9c648f9b8e8e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.2-prerelease")]
